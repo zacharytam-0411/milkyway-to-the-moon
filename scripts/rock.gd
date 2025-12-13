abe6e8b3bba28fc2a02a62ade43162d41b9204bf
Zachary <tlyzachary0411@gmail.com>
2025-12-13 22:56:38 +0800

    added emerald

--- scripts/rock.gd
+++ scripts/rock.gd
@@ -6,7 +6,9 @@ extends Area2D
 
 func _process(delta):
 	rotation_degrees += deg_to_rad(dps) * delta
-	if Global.rock > 100000:
+	if Global.rock > 1000000:
+		rock.texture = preload("res://assets/emerald.png")
+	elif Global.rock > 100000:
 		rock.texture = preload("res://assets/zinc.png")
 	elif Global.rock > 10000:
 		rock.texture = preload("res://assets/gold.png")
@@ -20,11 +22,15 @@ func _process(delta):
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
 		Global._rock_1click()
-		if Global.rock > 100000:
-			Global.rock1mult += ((Global.rock-100)*0.007)
+		if Global.rock > 1000000:
+			Global.rock1mult += ((Global.rock-500000)*0.009)
+		elif Global.rock > 100000:
+			Global.rock1mult += ((Global.rock-50000)*0.007)
 		elif Global.rock > 10000:
-			Global.rock1mult += ((Global.rock-100)*0.005)
+			Global.rock1mult += ((Global.rock-5000)*0.005)
 		elif Global.rock > 1000:
-			Global.rock1mult += ((Global.rock-100)*0.002)
+			Global.rock1mult += ((Global.rock-500)*0.002)
 		elif Global.rock > 100:
-			Global.rock1mult += ((Global.rock-100)*0.001)
+			Global.rock1mult += ((Global.rock-50)*0.001)
+		else:
+			Global.rock1mult += ((Global.rock-10)*0.0005)
